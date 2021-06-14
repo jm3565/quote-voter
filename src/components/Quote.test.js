3e257a07b8db8f2aa9cdd933c263c92ed12f3289
Juan <jnmunoz74@gmail.com>
2021-06-14 14:34:58 -0400

    Update quote comp tests

--- src/components/Quote.test.js
+++ src/components/Quote.test.js
@@ -2,38 +2,34 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import Quote from './Quote';
 
+const quoteVotes = jest.fn(() => null);
+const handleVoteChange = jest.fn();
+
 test('should render quote component with given text', () => {
-    const { container, getAllByTestId } = render(<Quote text="Test quote" onVoteChange={()=>{}}/>);
+    const { container, getAllByTestId } = render(<Quote id='storage-key' text="Test quote" votes={quoteVotes} onVoteChange={handleVoteChange}/>);
+
     expect(container).toBeInTheDocument();
 
     let quoteText = getAllByTestId('quote-txt');
     expect(quoteText[0].textContent).toBe("\"Test quote\"");
+
+    const countLabel = getAllByTestId('quote-votes')[0];
+    expect(countLabel.textContent).toBe('0');
 });
 
-test('clicking the "Upvote" button should increment the number of votes for the quote', () => {
-    const { getAllByTestId } = render(<Quote text="Test quote" onVoteChange={()=>{}}/>);
+test('clicking the "Upvote" button should increment the number of votes for the quote', async () => {
+    const { getAllByTestId } = render(<Quote id='storage-key' text="Test quote" votes={quoteVotes} onVoteChange={handleVoteChange}/>);
     const upVoteBtn = getAllByTestId('upvote-btn')[0];
-    const countLabel = getAllByTestId('quote-votes')[0];
-    
 
     userEvent.click(upVoteBtn);
-    expect(countLabel.textContent).toBe('1');
-    userEvent.click(upVoteBtn);
-    userEvent.click(upVoteBtn);
-    userEvent.click(upVoteBtn);
-    expect(countLabel.textContent).toBe('4');
+    expect(handleVoteChange).toHaveBeenCalledWith('storage-key', 1);
 });
 
 
 test('clicking the "Downvote" button should decrement the number of votes for the quote', () => {
-    const { getAllByTestId } = render(<Quote text="Test quote" onVoteChange={()=>{}}/>);
+    const { getAllByTestId } = render(<Quote id='storage-key' text="Test quote" votes={quoteVotes} onVoteChange={handleVoteChange}/>);
     const downVoteBtn = getAllByTestId('downvote-btn')[0];
-    const countLabel = getAllByTestId('quote-votes')[0];
 
     userEvent.click(downVoteBtn);
-    expect(countLabel.textContent).toBe('-1');
-    userEvent.click(downVoteBtn);
-    userEvent.click(downVoteBtn);
-    userEvent.click(downVoteBtn);
-    expect(countLabel.textContent).toBe('-4');
+    expect(handleVoteChange).toHaveBeenCalledWith('storage-key', -1);
 });
